dd8d9bf2a236dfc67aa385a093a6b20bed064a80
Dev Joe <joe.afify95@gmail.com>
2021-10-28 01:31:37 +0200

    Prepaired routes and pages.

---
+++ coach-finder/src/router.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
+import CoachesRegistration from './pages/coaches/CoachRegistration.vue';
+
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/coaches'
+    },
+    {
+      path: '/coaches',
+      component: CoachesList
+    },
+    {
+      path: '/coaches:id',
+      component: CoachDetails,
+      children: [
+        {
+          path: '/contact',
+          component: ContactCoach
+        }
+      ]
+    },
+    {
+      path: '/register',
+      component: CoachesRegistration
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound
+    }
+  ]
+});
+
+export default router;
